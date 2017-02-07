33d1694f527329ef5fbf867c6eece2e54dbf5208
serge <you@example.com>
2017-02-07 16:35:25 +0200

    added start-kit install packeges

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+#install neccessary apps and libs
+sudo apt-get install build-essential openssh-server gedit-plugins unzip zip subversion libssl-dev git gitk minicom autoconf automake gnupg 
+sudo apt-get install cmake flex bison gperf zip curl gcc-multilib g++-multilib libtool wget gparted ssh ncurses-dev u-boot-tools lzop 
+sudo apt-get install module-init-tools realpath openconnect phablet-tools device-tree-compiler 
+sudo apt-get update 
+
+
 dir=$PWD
 #list of files to make symlink to them:
 files=".bash_aliases"
-
 
 
 for file in $files; do

64fa0a4222908c87d49c48508bfe5fc4447ca2d4
Sergey Buloshnikov <sergeyx.buloshnikov@intel.com>
2017-02-08 15:27:23 +0200

    added new files, minor changes

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #install neccessary apps and libs
-sudo apt-get install build-essential openssh-server gedit-plugins unzip zip subversion libssl-dev git gitk minicom autoconf automake gnupg 
-sudo apt-get install cmake flex bison gperf zip curl gcc-multilib g++-multilib libtool wget gparted ssh ncurses-dev u-boot-tools lzop 
-sudo apt-get install module-init-tools realpath openconnect phablet-tools device-tree-compiler 
+#sudo apt-get install build-essential openssh-server gedit-plugins unzip zip subversion libssl-dev git gitk minicom autoconf automake gnupg 
+#sudo apt-get install cmake flex bison gperf zip curl gcc-multilib g++-multilib libtool wget gparted ssh ncurses-dev u-boot-tools lzop 
+#sudo apt-get install module-init-tools realpath openconnect phablet-tools device-tree-compiler 
 sudo apt-get update 
 
 
@@ -14,5 +14,8 @@ files=".bash_aliases .gitconfig .vimrc"
 
 for file in $files; do
 	echo "Creating symlink to $file in user's home directory"
-	ln -s $dir/$file ~/$file
+	ln -s $dir/$file ~/$file 2>/dev/null
 done
+
+echo ""
+echo "Finished"

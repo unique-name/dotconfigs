69fe481b3782a7454d74035d47098b849543dc7c
Sergey Buloshnikov <sergeyx.buloshnikov@intel.com>
2017-02-14 16:16:51 +0200

    fixed gitconfig, added support for vim plugins

--- .vimrc
+++ .vimrc
@@ -1,11 +1,39 @@
 set number
 set noswapfile
-syntax on
-filetype plugin indent on
 colorscheme monokai
 set autoindent
-set ruler
-set wildmenu
+"set ruler	 	" shows line number in the status line	 
+set wildmenu		" better command line completion, shows a list of matches
 set showcmd 
 set showmatch
 set matchtime=5
+set nocompatible	"need for plugins supporting 
+
+"turning off before plugins installation
+syntax off
+filetype plugin indent off
+set rtp+=~/.vim/bundle/vundle/
+"installs plugins from list below
+call vundle#begin()
+
+Plugin 'scrooloose/nerdtree.git'
+Plugin 'Valloric/YouCompleteMe'
+
+"all plugins must be added before this line
+call vundle#end() 
+"turning on after vundle finished
+syntax on
+filetype plugin indent on
+
+"-----------------------
+" NerdTree customization
+" ----------------------
+"autocmd StdinReadPre * let s:std_in=1
+"autocmd VimEnter * if argc() == 0 && !exists("s:std_in") | NERDTree | endif
+"hotkey to expand tree
+map <C-n> :NERDTreeToggle<CR>
+let NERDTreeIgnore=['.*\.o$']
+let NERDTreeIgnore+=['.*\.d$']
+let NERDTreeIgnore+=['.*\~$']
+let NERDTreeIgnore+=['.*\.out$']
+let NERDTreeIgnore+=['.*\.so$', '.*\.a$']
--- setup.sh
+++ setup.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+#need for vim updates
+#sudo add-apt-repository ppa:jonathonf/vim
+#sudo apt-get update
+
 #install neccessary apps and libs
-#sudo apt-get install build-essential openssh-server gedit-plugins unzip zip subversion libssl-dev git gitk minicom autoconf automake gnupg 
-#sudo apt-get install cmake flex bison gperf zip curl gcc-multilib g++-multilib libtool wget gparted ssh ncurses-dev u-boot-tools lzop 
+#sudo apt-get install ubuntu-restricted-extras vim vim-nox build-essential openssh-server gedit-plugins unzip zip subversion libssl-dev git gitk minicom 
+#sudo apt-get install cmake flex bison gperf zip curl gcc-multilib g++-multilib libtool wget gparted ssh ncurses-dev u-boot-tools lzop autoconf automake gnupg 
 #sudo apt-get install module-init-tools realpath openconnect phablet-tools device-tree-compiler 
 #sudo apt-get update 
-
 
 dir=$PWD
 #list of files to make symlink to them:
 files=".bash_aliases .gitconfig .vimrc .vim"
 
+#installing additional features:
+if [ ! -d "$dir/.vim/bundle/vundle" ]; then
+	echo "Installing vundle for vim"
+	git clone https://github.com/gmarik/vundle.git .vim/bundle/vundle 1>/dev/null
+fi
 
 for file in $files; do
 	echo "Creating symlink to $file in user's home directory"
@@ -18,4 +26,5 @@ for file in $files; do
 done
 
 echo ""
+echo "In vim enter the command: :PluginInstall"
 echo "Finished"

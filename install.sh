8aaa0ab28bd5da4e2f2e63a2a7b6e9c134e947e6
Sergey Buloshnikov <sergeyx.buloshnikov@intel.com>
2017-02-23 10:17:49 +0200

    added commands to commands file, updated install script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get install ubuntu-restricted-extras vlc vim vim-nox build-essential openssh-server gedit-plugins unzip zip subversion libssl-dev git gitk minicom 
 sudo apt-get install cmake flex bison gperf zip curl gcc-multilib g++-multilib libtool wget gparted ssh ncurses-dev u-boot-tools lzop autoconf automake gnupg 
-sudo apt-get install module-init-tools realpath openconnect phablet-tools device-tree-compiler python-dev libbz2-dev
+sudo apt-get install module-init-tools realpath openconnect phablet-tools device-tree-compiler python-dev libbz2-dev icu-devtools libicu-dev autotools-dev
 sudo apt-get update 
 
 #install java

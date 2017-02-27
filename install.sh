24aa66a5f56a11e045d12a5538811ce2398c014d
Sergey Buloshnikov <sergeyx.buloshnikov@intel.com>
2017-02-27 22:52:14 +0200

    updated install script

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
    
 sudo apt-get update
-sudo apt-get install ubuntu-restricted-extras vlc vim vim-nox build-essential openssh-server gedit-plugins unzip zip subversion libssl-dev git gitk minicom 
-sudo apt-get install cmake flex bison gperf zip curl gcc-multilib g++-multilib libtool wget gparted ssh ncurses-dev u-boot-tools lzop autoconf automake gnupg 
-sudo apt-get install module-init-tools realpath openconnect phablet-tools device-tree-compiler python-dev libbz2-dev icu-devtools libicu-dev autotools-dev
-sudo apt-get update 
+sudo apt-get -y install ubuntu-restricted-extras vlc vim vim-nox build-essential openssh-server gedit-plugins unzip zip subversion libssl-dev git gitk minicom 
+sudo apt-get -y install cmake flex bison gperf zip curl gcc-multilib g++-multilib libtool wget gparted ssh ncurses-dev u-boot-tools lzop autoconf automake gnupg 
+sudo apt-get -y install module-init-tools realpath openconnect phablet-tools device-tree-compiler python-dev libbz2-dev icu-devtools libicu-dev autotools-dev
 
 #install java
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java7-installer
-#sudo apt-get install oracle-java8-installer
+sudo apt-get -y install oracle-java7-installer

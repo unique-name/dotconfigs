c22191ed5e3d416d96773d0610fb579b72d02f6f
Sergey Buloshnikov <sergeyx.buloshnikov@intel.com>
2017-03-09 14:07:01 +0200

    updated install script and commands file

--- install.sh
+++ install.sh
@@ -2,10 +2,13 @@
    
 sudo apt-get update
 sudo apt-get -y install ubuntu-restricted-extras vlc vim vim-nox build-essential openssh-server gedit-plugins unzip zip subversion libssl-dev git gitk minicom 
-sudo apt-get -y install cmake flex bison gperf zip curl gcc-multilib g++-multilib libtool wget gparted ssh ncurses-dev u-boot-tools lzop autoconf automake gnupg 
-sudo apt-get -y install module-init-tools realpath openconnect phablet-tools device-tree-compiler python-dev libbz2-dev icu-devtools libicu-dev autotools-dev
+sudo apt-get -y install cmake flex bison gperf zip curl gawk gcc-multilib g++-multilib libtool wget gparted ssh ncurses-dev u-boot-tools lzop autoconf automake gnupg 
+sudo apt-get -y install module-init-tools realpath openconnect phablet-tools device-tree-compiler python-dev libbz2-dev icu-devtools libicu-dev autotools-dev expect ccache
+sudo apt-get -y install zlib1g:i386 libstdc++6:i386 lib32stdc++6
 
 #install java
-sudo add-apt-repository ppa:webupd8team/java
+#sudo add-apt-repository ppa:webupd8team/java
+#sudo apt-get update
+#sudo apt-get -y install oracle-java7-installer
+#sudo apt-get install openjdk-7-jre-headless
 sudo apt-get update
-sudo apt-get -y install oracle-java7-installer

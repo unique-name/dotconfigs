c4da9665042c28e4f80da785d7aabf0bbb16998c
Sergey Buloshnikov <sergeyx.buloshnikov@intel.com>
2017-04-05 11:44:36 +0300

    updated commands file

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get -y install ubuntu-restricted-extras vlc vim vim-nox build-essential openssh-server gedit-plugins unzip zip subversion libssl-dev git gitk minicom 
 sudo apt-get -y install cmake flex bison gperf zip curl gawk gcc-multilib g++-multilib libtool wget gparted ssh ncurses-dev u-boot-tools lzop autoconf automake gnupg 
 sudo apt-get -y install module-init-tools realpath openconnect phablet-tools device-tree-compiler python-dev libbz2-dev icu-devtools libicu-dev autotools-dev expect ccache
-sudo apt-get -y install zlib1g:i386 libstdc++6:i386 lib32stdc++6
+sudo apt-get -y install zlib1g:i386 libstdc++6:i386 lib32stdc++6 libisl-dev
 
 #install java
 #sudo add-apt-repository ppa:webupd8team/java

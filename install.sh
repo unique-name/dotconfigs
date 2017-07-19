f0363746127b10d3124bba45d48dbec367bb6f51
Sergey Buloshnikov <sergeyx.buloshnikov@intel.com>
2017-07-19 18:13:46 +0300

    Added packages for Yocto compilation
    Signed-off-by: Sergey Buloshnikov <sergeyx.buloshnikov@intel.com>

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
    
 sudo apt-get update
-sudo apt-get -y install ubuntu-restricted-extras vlc vim vim-nox build-essential openssh-server gedit-plugins unzip zip subversion libssl-dev git gitk minicom 
-sudo apt-get -y install cmake flex bison gperf zip curl gawk gcc-multilib g++-multilib libtool wget gparted ssh ncurses-dev u-boot-tools lzop autoconf automake gnupg 
-sudo apt-get -y install module-init-tools realpath openconnect phablet-tools device-tree-compiler python-dev libbz2-dev icu-devtools libicu-dev autotools-dev expect ccache
-sudo apt-get -y install zlib1g:i386 libstdc++6:i386 lib32stdc++6 libisl-dev
+sudo apt autoremove
+sudo apt-get -y install ssh meld ubuntu-restricted-extras texinfo
+sudo apt-get -y install build-essential vlc vim vim-nox openssh-server gedit-plugins unzip zip subversion libssl-dev git gitk minicom diffstat chrpath socat
+sudo apt-get -y install cmake flex bison gperf zip curl gawk gcc-multilib g++-multilib libtool wget gparted ncurses-dev u-boot-tools lzop autoconf automake gnupg 
+sudo apt-get -y install module-init-tools realpath openconnect phablet-tools device-tree-compiler python-dev icu-devtools autotools-dev expect ccache
+sudo apt-get -y install zlib1g:i386 libstdc++6:i386 lib32stdc++6 libisl-dev libsdl1.2-dev libbz2-dev libicu-dev
 
 #install java
 #sudo add-apt-repository ppa:webupd8team/java

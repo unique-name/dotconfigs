e43140c821d39053beed6792f9ac7f3d15c9cfbc
Sergey Buloshnikov <sergey.buloshnikov@osrenterprises.com>
2018-01-02 15:59:10 +0200

    install script: updated script
    Signed-off-by: Sergey Buloshnikov <sergey.buloshnikov@osrenterprises.com>

--- install.sh
+++ install.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
    
 sudo apt-get update
+sudo apt-get upgrade
 sudo apt autoremove
-sudo apt-get -y install ssh meld ubuntu-restricted-extras texinfo python-pip colordiff ntfs-config
-sudo apt-get -y install build-essential vlc vim vim-nox openssh-server gedit-plugins unzip zip subversion libssl-dev git gitk minicom diffstat chrpath socat
-sudo apt-get -y install cmake flex bison gperf zip curl gawk gcc-multilib g++-multilib libtool wget gparted ncurses-dev u-boot-tools lzop autoconf automake gnupg 
-sudo apt-get -y install module-init-tools realpath openconnect repo device-tree-compiler python-dev icu-devtools autotools-dev expect ccache
-sudo apt-get -y install zlib1g:i386 libstdc++6:i386 lib32stdc++6 libisl-dev libsdl1.2-dev libbz2-dev libicu-dev
+sudo apt-get -y install ssh meld ubuntu-restricted-extras texinfo python-pip
+sudo apt-get -y install scolordiff ntfs-config libboost-all-dev unzip zip
+sudo apt-get -y install build-essential vlc vim vim-nox openssh-server socat
+sudo apt-get -y install gedit-plugins git gitk minicom diffstat chrpath ccache
+sudo apt-get -y install libssl-dev subversion libtool wget gparted ncurses-dev
+sudo apt-get -y install cmake flex bison gperf zip curl gawk gcc-multilib  
+sudo apt-get -y install g++-multilib u-boot-tools lzop autoconf automake gnupg
+sudo apt-get -y install module-init-tools realpath openconnect repo python-dev
+sudo apt-get -y install device-tree-compiler icu-devtools autotools-dev expect 
+sudo apt-get -y install exuberant-ctags
+sudo apt-get -y install zlib1g:i386 libstdc++6:i386 lib32stdc++6 libisl-dev 
+sudo apt-get -y install libsdl1.2-dev libbz2-dev libicu-dev
 
 #install java
 #sudo add-apt-repository ppa:webupd8team/java
 #sudo apt-get update
 #sudo apt-get -y install oracle-java7-installer
 #sudo apt-get install openjdk-7-jre-headless
-sudo apt-get upgrade

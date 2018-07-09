31f516a0b0073b92b680082ec04915ea12600882
Sergey Buloshnikov <sergey.buloshnikov@osrenterprises.com>
2018-07-09 15:01:05 +0300

    reworked install script
    Signed-off-by: Sergey Buloshnikov <sergey.buloshnikov@osrenterprises.com>

--- install.sh
+++ install.sh
@@ -3,21 +3,21 @@
 sudo apt update
 sudo apt -y upgrade
 sudo apt -y autoremove
-sudo apt -y install ssh meld ubuntu-restricted-extras python-pip net-tools
+sudo apt -y install ssh meld ubuntu-restricted-extras python-pip python-dev net-tools
 sudo apt -y install scolordiff ntfs-config libboost-all-dev zip
 sudo apt -y install vlc vim-gnome vim-nox openssh-server exuberant-ctags libgnome2-bin
 sudo apt-get -y install gedit-plugins gitk minicom ccache
+
 # Packages for Yocto host development
 sudo apt -y install gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential \
 	chrpath socat xterm cpio python3 python3-pip libsdl1.2-dev libelf-dev
-sudo apt -y install libssl-dev subversion libtool gparted ncurses-dev
-sudo apt -y install cmake flex bison gperf zip curl
-sudo apt -y install g++-multilib u-boot-tools lzop autoconf automake gnupg
-sudo apt -y install module-init-tools realpath openconnect repo python-dev
-sudo apt -y install device-tree-compiler icu-devtools autotools-dev expect
 
-#sudo apt-get -y install zlib1g:i386 libstdc++6:i386 lib32stdc++6 libisl-dev
-#sudo apt-get -y install libsdl1.2-dev libbz2-dev libicu-dev
+sudo apt -y install libssl-dev subversion libtool gparted ncurses-dev libicu-dev
+sudo apt -y install g++-multilib cmake bison flex zip curl repo
+
+# Packages for uboot development
+sudo apt -y install u-boot-tools lzop autoconf automake gnupg module-init-tools realpath \
+	openconnect device-tree-compiler icu-devtools autotools-dev expect
 
 # install android tools
 #sudo apt -y install android-tools-adb android-tools-fastboot

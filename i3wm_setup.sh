520dc0b5e9d7739646398132a7b60f288fdb7496
Sergey Buloshnikov <sergey.buloshnikov@osrenterprises.com>
2019-03-17 11:46:22 +0200

    i3wm_setup: fixed thunar problem with icons
    Signed-off-by: Sergey Buloshnikov <sergey.buloshnikov@osrenterprises.com>

--- i3wm_setup.sh
+++ i3wm_setup.sh
@@ -7,6 +7,13 @@ sudo apt -y install i3 i3status
 sudo apt -y install thunar thunar-data gnome-icon-theme
 dir=$PWD
 
+files=".gtkrc-2.0"
+
+for file in $files; do
+	echo "Creating symlink to $file in user's home directory"
+	ln -fs $dir/$file ~/$file 2>/dev/null
+done
+
 if [ ! -d "~/.config/i3" ]; then
 	mkdir ~/.config/i3 2>/dev/null
 	ln -s $dir/i3/config ~/.config/i3/config 2>/dev/null

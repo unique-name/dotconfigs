503bec7e3533c6ce1befdc65560e91a8c8e05275
Sergey Buloshnikov <sergeybu@essence-grp.com>
2020-01-14 13:52:08 +0200

    reworked install script
    Signed-off-by: Sergey Buloshnikov <sergeybu@essence-grp.com>

--- install.sh
+++ install.sh
@@ -3,23 +3,22 @@
 sudo apt update
 sudo apt -y upgrade
 sudo apt -y autoremove
-sudo apt -y install ssh meld ubuntu-restricted-extras python-pip python-dev libjsoncpp-dev
-sudo apt -y install colordiff ntfs-config libboost-dev libboost-all-dev libpcap-dev zip
-sudo apt -y install vlc vim-gnome vim-nox openssh-server exuberant-ctags libgnome2-bin
-sudo apt -y install gedit-plugins gitk minicom ccache
+sudo apt -y install ssh meld ubuntu-restricted-extras libjsoncpp-dev gedit-plugins gitk minicom
+sudo apt -y install colordiff ntfs-config libboost-dev libboost-all-dev libpcap-dev zip ccache
+sudo apt -y install vlc vim-gnome vim-nox openssh-server exuberant-ctags libgnome2-bin autotools-dev
 sudo apt -y install libssl-dev subversion libtool gparted libncurses5-dev libncursesw5-dev
-sudo apt -y install g++-multilib cmake bison flex curl repo libicu-dev autoconf automake autotools-dev
+sudo apt -y install g++-multilib cmake bison flex curl repo libicu-dev autoconf automake
 
 # Packages for Yocto host development
-sudo apt -y install gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential \
-	chrpath socat xterm cpio python3 python3-pip libsdl1.2-dev libelf-dev
-
+sudo apt -y install gawk wget git diffstat unzip texinfo gcc-multilib build-essential \
+	chrpath socat xterm cpio python3 libsdl1.2-dev libelf-dev
 
 # Packages for uboot development
-#sudo apt -y install u-boot-tools lzop gnupg module-init-tools openconnect device-tree-compiler icu-devtools expect
+#sudo apt -y install u-boot-tools lzop gnupg module-init-tools openconnect device-tree-compiler
+#udo apt -y install icu-devtools expect
 
 # install android tools
-#sudo apt -y install android-tools-adb android-tools-fastboot
+sudo apt -y install android-tools-adb android-tools-fastboot
 
 # install audio cutters/converters
 #sudo apt -y install sox libsox-fmt-mp3
@@ -27,8 +26,3 @@ sudo apt -y install gawk wget git-core diffstat unzip texinfo gcc-multilib build
 # install ebook converter
 #sudo apt -y install calibre
 
-# install java
-#sudo add-apt-repository ppa:webupd8team/java
-#sudo apt-get update
-#sudo apt-get -y install oracle-java7-installer
-#sudo apt-get install openjdk-7-jre-headless

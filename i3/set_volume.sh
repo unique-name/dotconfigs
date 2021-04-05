e4302cd71ecb845960b214b8d0baab6a0cba021c
Sergey Buloshnikov <sergeybu@essence-grp.com>
2021-04-05 10:00:12 +0300

    i3: setting volume for all devices (include sound over usb)
    Signed-off-by: Sergey Buloshnikov <sergeybu@essence-grp.com>

--- i3/set_volume.sh
+++ i3/set_volume.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sink=$(pactl list short sinks | awk '{print $1;}')
-
-case "$1" in
-	up ) pactl set-sink-volume $sink +2% ;;
-	down ) pactl set-sink-volume $sink -2% ;;
-	mute ) pactl set-sink-mute $sink toggle ;;
-esac
+pactl list short sinks | awk '{print $1;}' | while read devid; do
+	case "$1" in
+		up ) pactl set-sink-volume $devid +2% ;;
+		down ) pactl set-sink-volume $devid -2% ;;
+		mute ) pactl set-sink-mute $devid toggle ;;
+	esac
+done
 

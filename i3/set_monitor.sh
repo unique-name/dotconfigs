f87999557082c108e47acfbb497029b98fde39cd
Sergey Buloshnikov <sergeybu@essence-grp.com>
2021-05-24 00:06:21 +0300

    i3: updated script that disables laptop screen in case HDMI
    Signed-off-by: Sergey Buloshnikov <sergeybu@essence-grp.com>

--- i3/set_monitor.sh
+++ i3/set_monitor.sh
@@ -11,3 +11,6 @@ fi
 if [[ `xrandr | grep HDMI1 | awk  '{print $2}'` = 'connected' ]]; then
 xrandr --output eDP1 --off;
 fi
+if [[ `xrandr | grep HDMI-2 | awk  '{print $2}'` = 'connected' ]]; then
+xrandr --output eDP-1 --off;
+fi
